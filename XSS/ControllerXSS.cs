1d3406a7139d58ec865b2c0254e9a5b695b81b0b
Nihad Alasgarov <nihadelesgerov0@gmail.com>
2025-03-03 12:52:44 +0400

    XSRF protection information updated :)

--- XSS/ControllerXSS.cs
+++ XSS/ControllerXSS.cs
@@ -5,16 +5,14 @@ namespace AvoidDoS.XSS
     public class ControllerXSS : Controller
     {
 
-        // Let's say you get data from use via input
-
-
-        // ENSURE YOU ARE USING HTMLSANITIZER => Nuget Packages => AspNetHTMLSanitizer package 
+        
 
 
         public IActionResult AvoidXssMethod(TestModel info)
         {
+            // Nuget Package Called HtmlSanitizer process input to convert fully text file that is not able to perform :)
+            // Human Facing applications must use it 
             var sanitizedInfo = new HtmlSanitizer().Sanitize(info.UserName);
-            // that's it . Data sanitizered already
             return View();
         }
     }
--- XSS/TestModel.cs
+++ XSS/TestModel.cs
@@ -5,10 +5,13 @@ namespace AvoidDoS.XSS
     public class TestModel
     {
         [Required]
-        // Almost always length of scripts longer than 100
+        // Almost always length of scripts become longer than 100
         // Use model validation to restrict input to expected formats
 
         [StringLength(100,ErrorMessage ="Input too long")]
         public string  UserName { get; set; }
+
+        //@HTML.Raw() which takes input to server raw,if malicous user tries to send script code to our server, code will be able to perform something, because html is able process input, without worrying about it.
+        // To avoid this, we don't have to use @HTML.Raw() in our form, so html will treat input as a text and server doing nothing with this malicious input :)
     }
 }
